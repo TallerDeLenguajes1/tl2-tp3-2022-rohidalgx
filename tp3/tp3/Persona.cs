12d5af123b28ce9ace44cd8abb91e818f0531a2c
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-11-11 17:38:24 -0300

    por fin termine gracias Dios

---
+++ tp3/tp3/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Persona
+{
+    int ID;
+    string Nombre;
+    string Direccion;
+    long telefono;
+
+
+    public Persona(int id, string nombre, string direccion, long telefono)
+    {
+        this.ID = id;
+        this.Nombre = nombre;
+        this.Direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public int getID()
+    {
+        return (this.ID);
+    }
+}
+
---
+++ tp3/tp3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using NLog;
+
+Console.WriteLine("Hello");
+
+Helper miAyudador = new Helper();
+List<Cadete> cadetes = new List<Cadete>();
+cadetes = miAyudador.LeerCSVYCrearListaDeCadetes("Cadete.csv");
+Cadeteria miCadeteria = new Cadeteria("Envios", 4350358, cadetes);
+Console.WriteLine("BIENVENIDO");
+Console.WriteLine("Menú alta de pedidos");
+Pedido pedido = miCadeteria.altaPedido(1, "Juan", "JBJ 5", 381523654, "Porton negro", "ninguna", false);
+Console.WriteLine("Menú asignar cadete");
+miCadeteria.asignarPedido(1, pedido);
+Console.WriteLine("Menú cambiar estado");
+
+pedido.cambiarEstadoPedido(true);
+Console.WriteLine("Menú cambiar cadete");
+miCadeteria.reasignarPedido(cadetes[1], cadetes[2], 1);
+
+Console.WriteLine("Menú seleccion salir");
+miCadeteria.mostrarInforme();
+
+
+
